c33d0e2a5835754744cbf0ff36615271c3099239
SelinJodhani <jodhaniselin.sj@gmail.com>
2022-03-16 18:40:06 +0530

    Getting started with socket.io

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
+    allowEIO3: true,
+  },
+  transport: ['websocket'],
+});
+
 dotenv.config({ path: './.env' });
 
 const DATABASE_STRING = process.env.MONGO_CONNECTION_STRING.replace(
@@ -13,6 +25,18 @@ mongoose.connect(DATABASE_STRING, () => {
   console.log('Database connection successfull.');
 });
 
-app.listen(process.env.PORT || 8000, () => {
+io.on('connection', socket => {
+  socket.emit('connected');
+
+  socket.on('join-channel', channelSlug => {
+    socket.join(channelSlug);
+  });
+
+  socket.on('message', data => {
+    socket.to(data.channelSlug).emit('newMessage', data);
+  });
+});
+
+server.listen(process.env.PORT || 8000, () => {
   console.log(`Server is listening to PORT: ${process.env.PORT}`);
 });
